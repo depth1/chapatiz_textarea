0ee8cef26f57d0cfc2bbe8f1acc1f4ca7051f05f
depth1 <depth1@protonmail.com>
2019-01-11 09:27:38 +0100

    Fix matching URL Change with updated URL.

--- inject.js
+++ inject.js
@@ -8,7 +8,6 @@ chrome.storage.sync.get(
         let dom_subject = document.getElementsByName("subject")[0];
         let dom_message = document.getElementsByName("message")[0];
 
-        console.log(result.chz_bbs_subject);
         if(result.chz_bbs_subject != undefined && dom_subject.value.length == 0) dom_subject.value = result.chz_bbs_subject;
         if(result.chz_bbs_message != undefined && dom_message.value.length == 0) dom_message.value = result.chz_bbs_message;
     }
